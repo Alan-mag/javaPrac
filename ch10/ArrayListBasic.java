6acb10de6a09fc1573d69d0cd1c8898db5765df8
Alan Magelssen <alanmagelssen@gmail.com>
2017-08-31 14:01:24 -0700

    ch10

---
+++ ch10/ArrayListBasic.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class ArrayListBasic {
+ public static void main(String[] args) {
+    ArrayList<String> list = new ArrayList<String>();   
+    list.add("jorge");
+    list.add("hector");
+    System.out.println(list);
+    System.out.println(list.size());
+ }
+
+}
