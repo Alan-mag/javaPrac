badf1b6818355296e34a7fb1639284958e42a3d1
Alan Magelssen <alanmagelssen@gmail.com>
2017-09-22 14:07:24 -0700

    ch10 text and 11 beginning

---
+++ ch11/LinkedPrac.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class LinkedPrac {
+    public static void main(String[] args) {
+        LinkedList<String> words = new LinkedList<String>();
+        words.add("hello");
+        words.add("goodbye");
+        words.add("this");
+        words.add("that");
+        System.out.println(words);
+    }
+}
